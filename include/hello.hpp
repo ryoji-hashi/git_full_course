6dc5ba8ec1a21008df40ca023c05c1c01dbd9164
ryoji-hashi <kurahashi_ryoji@stu.kobe-u.ac.jp>
2025-04-21 03:45:42 +0900

    CMakeによるbuildができた

---
+++ include/hello.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int hello(){
+    std::cout << "[hello] started!" << std::endl;
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+#include "hello.hpp"
+
+int main(){
+    std::cout << "hello, world!" << std::endl;
+    hello();
+    return 0;
+}
