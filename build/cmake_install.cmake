69784232235e578df79995fec2474c14c66da6df
ryoji-hashi <kurahashi_ryoji@stu.kobe-u.ac.jp>
2025-04-21 14:39:05 +0900

    2つめの.exeファイル，sample2を作成した．

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/cmake_test")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/cmake_test")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
---
+++ src/sample2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdio.h>
+
+int main(){
+    printf("Yes! I got to make the second .exe file!");
+
+    return 0;
+}
